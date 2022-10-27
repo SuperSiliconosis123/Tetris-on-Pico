8aa588dfe002840d7813f70051f08276d7f9bac0
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2022-10-27 16:02:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,18 +25,17 @@ while True:
         lcd.vline(186, 50, 40, 65535)
         lcd.vline(206, 50, 40, 65535)
         # draw highscore grid
-        lcd.rect(125, 135, 102, 60, 65535)
-        lcd.vline(180, 135, 60, 65535)
-        lcd.hline(125, 150, 102, 65535)
-        lcd.hline(125, 165, 102, 65535)
-        lcd.hline(125, 180, 102, 65535)
+        lcd.rect(120, 135, 112, 60, 65535)
+        lcd.vline(175, 135, 60, 65535)
+        lcd.hline(120, 150, 112, 65535)
+        lcd.hline(120, 165, 112, 65535)
+        lcd.hline(120, 180, 112, 65535)
         printstring(" NAME  SCORE" ,129, 139, 1, 65535)
         # get high scores for the grid
-        f = open("highscore")
-        printstring(f.readline().rstrip('\n'), 129, 154, 1, 65535)
-        printstring(f.readline().rstrip('\n'), 129, 169, 1, 65535)
-        printstring(f.readline().rstrip('\n'), 129, 184, 1, 65535)
-        f.close()
+        with open("highscore") as f:
+            printstring(f.readline().rstrip('\n'), 124, 154, 1, 65535)
+            printstring(f.readline().rstrip('\n'), 124, 169, 1, 65535)
+            printstring(f.readline().rstrip('\n'), 124, 184, 1, 65535)
         # draw help text
         printstring("X: HELP", 150, 220, 1, 65535)
         printstring("Y: CONTINUE", 133, 230, 1, 65535)
@@ -77,7 +76,9 @@ while True:
                 if left.value() == 0 and level != 0:
                     level -= 1
                     break
-                if keyX.value() == 0: tetris.help()
+                if keyX.value() == 0:
+                    tetris.help()
+                    break
                 if keyY.value() == 0:
                     play = True
                     break
@@ -87,7 +88,7 @@ while True:
     lcd.fill_rect(120, 0, 120, 255, 0)
     lcd.fill_rect(5, 5, 110, 30, 0)
     printstring("TETRIS", 5, 10, 2, 65535)
-    printstring("SCORE: " + '{:0>{w}}'.format(str(int(tetris.score)), w=5), 130, 5, 1, 65535)
+    printstring("SCORE: " + '{:0>{w}}'.format(str(int(tetris.score)), w=6), 130, 5, 1, 65535)
     printstring("LINES: " + '{:0>{w}}'.format(str(int(tetris.lines)), w=4), 135, 15, 1, 65535)
     printstring("LEVEL: " + str(level), 148, 25, 1, 65535)
     lcd.fill_rect(120, 40, 20, 10, 65535)# skew
@@ -246,39 +247,69 @@ while True:
     printstring("CONTINUE", 148, 220, 1, 65535)
     lcd.show()
     # get high scores for the grid
-    f = open("highscore")
-    line1 = f.readline().rstrip('\n')
-    line2 = f.readline().rstrip('\n')
-    line3 = f.readline().rstrip('\n')
-    f.close()
+    with open("highscore") as f:
+        line1 = f.readline().rstrip('\n')
+        line2 = f.readline().rstrip('\n')
+        line3 = f.readline().rstrip('\n')
     scores = {'score1': int(line1[7:12]), 'score2': int(line2[7:12]), 'score3': int(line3[7:12]), 'myscore': tetris.score}
     highscores = [key for (key, value) in sorted(scores.items(), key=lambda key_value: key_value[1])]
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-', '.']
     nameNumbers = [27, 27, 27, 27, 27, 27]
-    name = ''
-    name = name.join([alphabet[nameNumbers[i]] for i in range(6)])
-    if highscores.index('myscore') == 0:
+    inputIndex = 0
+    name = ''.join([alphabet[nameNumbers[i]] for i in range(6)])
+    if highscores.index('myscore') == 3:
         line3 = line2
         line2 = line1
-        line1 = str(name+' '+zfill(str(tetris.score), 5))
-    elif highscores.index('myscore') == 1:
-        line3 = line2
-        line2 = str(name+' '+zfill(str(tetris.score), 5))
+        line1 = str(name+' '+zfill(str(tetris.score), 6))
     elif highscores.index('myscore') == 2:
-        line3 = str(name+' '+zfill(str(tetris.score), 5))
+        line3 = line2
+        line2 = str(name+' '+zfill(str(tetris.score), 6))
+    elif highscores.index('myscore') == 1:
+        line3 = str(name+' '+zfill(str(tetris.score), 6))
     printstring(line1, 129, 154, 1, 65535)
     printstring(line2, 129, 169, 1, 65535)
     printstring(line3, 129, 184, 1, 65535)
     lcd.show()
+    highlight = False
+    c = colour(240, 240, 240)
+    if highscores.index('myscore') != 0:
+        while True:
+            highlight = not highlight
+            lcd.fill_rect(129, 153+((highscores.index('myscore')-3)*(-15)), 48, 10, 0)
+            if highlight == True: lcd.fill_rect(128+(inputIndex*8), 154+((highscores.index('myscore')-3)*(-15)), 8, 8, c)
+            else: lcd.fill_rect(128+(inputIndex*8), 154+((highscores.index('myscore')-3)*(-15)), 8, 8, 0)
+            printstring(name, 129, 154+((highscores.index('myscore')-3)*(-15)), 1, 65535)
+            lcd.show()
+            if utime.ticks_ms() - start > 150:
+                if up.value() == 0:
+                    nameNumbers[inputIndex] = (nameNumbers[inputIndex] + 1)%29
+                    name = ''.join([alphabet[nameNumbers[i]] for i in range(6)])
+                    start = utime.ticks_ms()
+                elif down.value() == 0:
+                    nameNumbers[inputIndex] = (nameNumbers[inputIndex] - 1)%29
+                    name = ''.join([alphabet[nameNumbers[i]] for i in range(6)])
+                    start = utime.ticks_ms()
+                elif right.value() == 0 and inputIndex != 5:
+                    inputIndex += 1
+                    start = utime.ticks_ms()
+                elif left.value() == 0 and inputIndex != 0:
+                    inputIndex -= 1
+                    start = utime.ticks_ms()
+                elif keyY.value() == 0: break
+    if highscores.index('myscore') == 3: line1 = str(name+' '+zfill(str(tetris.score), 5))
+    elif highscores.index('myscore') == 2: line2 = str(name+' '+zfill(str(tetris.score), 5))
+    elif highscores.index('myscore') == 1: line3 = str(name+' '+zfill(str(tetris.score), 5))
+    with open('highscore', 'w') as f:
+        f.write(line1+'\n'+line2+'\n'+line3+'\n')
     while True:
         if keyY.value() == 0: break
-    board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    tetris.board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
